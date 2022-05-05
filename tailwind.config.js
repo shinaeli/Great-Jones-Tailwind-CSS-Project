7e6cb443e51b501cd781e29c81d10f8f577db7a5
shinaeli <omotoshoelisha@gmail.com>
2022-05-05 20:53:17 +0100

    created a landing page and styled with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'mistyrose',
+        secondary: 'gold',
+      }
+    },
+  },
+  plugins: [],
+}
